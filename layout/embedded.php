c24ccce21f6e26641331410ef947c7f5bd0d8441
Danny Wahl <danny@iywahl.com>
2011-10-18 09:07:13 +0800

    new embedded layout

--- layout/embedded.php
+++ layout/embedded.php
@@ -22,10 +22,51 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-echo $OUTPUT->doctype() ?>
-<html <?php echo $OUTPUT->htmlattributes() ?>>
+$hasheading = ($PAGE->heading);
+$hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());
+$hasfooter = (empty($PAGE->layout_options['nofooter']));
+$hassidepre = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-pre', $OUTPUT));
+$hassidepost = (empty($PAGE->layout_options['noblocks']) && $PAGE->blocks->region_has_content('side-post', $OUTPUT));
+$haslogininfo = (empty($PAGE->layout_options['nologininfo']));
+$showsidepre = ($hassidepre && !$PAGE->blocks->region_completely_docked('side-pre', $OUTPUT));
+$showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT));
+$custommenu = $OUTPUT->custom_menu();
+$hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
+$bodyclasses = array();
+if ($showsidepre && !$showsidepost) {
+    $bodyclasses[] = 'side-pre-only';
+} else if ($showsidepost && !$showsidepre) {
+    $bodyclasses[] = 'side-post-only';
+} else if (!$showsidepost && !$showsidepre) {
+    $bodyclasses[] = 'content-only';
+}
+if ($hascustommenu) {
+    $bodyclasses[] = 'has_custom_menu';
+}
+if (!empty($PAGE->theme->settings->headeralt)) {
+    $headeralt = $PAGE->theme->settings->headeralt;
+} else {
+    $headeralt = $PAGE->heading;
+}
+if (!empty($PAGE->theme->settings->branding)) {
+    $branding = $PAGE->theme->settings->branding;
+} else {
+    $branding = 0;
+}
+if (!empty($PAGE->theme->settings->enablezoom)) {
+    $enablezoom = $PAGE->theme->settings->branding;
+    if ($enablezoom == 1) {
+         $zoomenabled = 'yes';
+    } else {
+         $zoomenabled = 'no';
+    }
+} else {
+    $zoomenabled = 'no';
+}
+echo $OUTPUT->doctype(); ?>
+<html <?php echo $OUTPUT->htmlattributes(); ?>>
 <head>
-    <title><?php echo $PAGE->title ?></title>
+    <title><?php echo $PAGE->title; ?></title>
     <!-- Mobile viewport optimization -->
     <meta name="HandheldFriendly" content="True">
     <meta name="MobileOptimized" content="480"/>
@@ -46,16 +87,101 @@ echo $OUTPUT->doctype() ?>
     <link rel="apple-touch-icon-precomposed" href="<?php echo $OUTPUT->pix_url('favicon/l/apple-touch-icon-precomposed', 'theme'); ?>">
     <!-- For nokia devices: -->
     <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon/l/apple-touch-icon-precomposed', 'theme'); ?>">
-    <?php echo $OUTPUT->standard_head_html() ?>
+<?php echo $OUTPUT->standard_head_html(); ?>
 </head>
-<body id="<?php echo $PAGE->bodyid ?>" class="<?php echo $PAGE->bodyclasses ?>">
-<?php echo $OUTPUT->standard_top_of_body_html() ?>
-
-<div id="page">
-    <div id="content" class="clearfix">
-        <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+<body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
+    <?php echo $OUTPUT->standard_top_of_body_html(); ?>
+    <div id="page">
+        <div id="page-inner-wrapper">
+            <div id="page-header-wrapper">
+                <div id="page-header" class="clearfix">
+                    <h1 class="headermain"><?php echo $headeralt; ?></h1>
+                    <div id="profileblock">
+                        <?php
+                        if (isloggedin()) {
+                            if ($haslogininfo) {
+                                echo html_writer::tag('div', $OUTPUT->user_picture($USER, array('size'=>80)), array('id'=>'user-pic'));
+                                echo $OUTPUT->login_info();
+                            }
+                            if (!empty($PAGE->layout_options['langmenu'])) {
+                                echo $OUTPUT->lang_menu();
+                            }
+                            echo $PAGE->headingmenu;
+                        } else {
+                            echo $OUTPUT->login_info();
+                        }
+                        ?>  
+                    </div>
+                </div>
+                <div id="page-border-wrapper">
+                    <?php if ($hascustommenu) { ?>
+                        <div id="custommenu-wrapper">
+                            <div id="custommenu"><?php echo $custommenu; ?></div>
+                        </div>
+                    <?php } ?>
+                    <?php if ($hasnavbar) { ?>
+                        <div id="navbar-wrapper">
+                            <div class="navbar clearfix">
+                                <div class="breadcrumb"><?php echo $OUTPUT->navbar(); ?></div>
+                                <div class="navbutton"> <?php echo $PAGE->button; ?></div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <div id="page-content-wrapper">
+                        <div id="page-content">
+                            <div id="region-main-box">
+                                <div id="region-post-box">
+                                    <div id="region-main-wrap">
+                                        <div id="region-main">
+                                            <div class="region-content">
+                                                <?php echo core_renderer::MAIN_CONTENT_TOKEN; ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php if ($hassidepre) { ?>
+                                        <div id="region-pre" class="block-region">
+                                            <div class="region-content">
+                                                <?php echo $OUTPUT->blocks_for_region('side-pre'); ?>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                    <?php if ($hassidepost) { ?>
+                                        <div id="region-post" class="block-region">
+                                            <div class="region-content">
+                                                <?php echo $OUTPUT->blocks_for_region('side-post'); ?>
+                                            </div>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php if ($hasfooter) { ?>
+                <div id="page-footer-wrapper">
+                    <div id="page-footer">
+                        <p class="helplink">
+                            <?php echo page_doc_link(get_string('moodledocslink')); ?>
+                        </p>
+                        <?php
+                        echo $OUTPUT->login_info();
+                        echo "<br />";
+                        echo $OUTPUT->standard_footer_html();
+                        if ($branding == 0) {
+                        echo '<div id="branding">';
+                        echo '<a href="http://ldichina.com"><img src="'.$OUTPUT->pix_url('footer/LDi', 'theme').'" alt="LDi China"></a>';
+                        echo '<a href="http://teachwithisc.com"><img src="'.$OUTPUT->pix_url('footer/iSC', 'theme').'" alt="International Schools of China"></a>';
+                        echo '<a href="http://tiseagles.com"><img src="'.$OUTPUT->pix_url('footer/TIS', 'theme').'" alt="Tianjin International School"></a>';
+                        echo '<a href="http://iyware.com"><img src="'.$OUTPUT->pix_url('footer/iyWare', 'theme').'" alt="iyWare.com"></a>';
+                        echo '</div>';
+                        }
+                        ?>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </div>
-</div>
-<?php echo $OUTPUT->standard_end_of_body_html() ?>
+    <?php echo $OUTPUT->standard_end_of_body_html(); ?>
 </body>
 </html>
