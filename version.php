99f0e3cb151cfa3cdd89eebe86f015d56c825832
Danny Wahl <danny@iywahl.com>
2011-12-05 13:29:02 +0800

    bump to version 2.1.1 stable

--- version.php
+++ version.php
@@ -24,7 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
  
-$plugin->version   = 2011111004;
+$plugin->version   = 2011120500;
 $plugin->component = 'theme_zebra';
 $plugin->requires  = 2010112400;
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->release = '2.1.1 (Build: 2011120500)';
