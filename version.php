c219183de426e68fd71c42918e1c0fca627a6de7
Danny Wahl <danny@iywahl.com>
2011-12-27 18:56:34 +0800

    bump required version to 2.2

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die;
  
 $plugin->version   = 2011120600;
 $plugin->component = 'theme_zebra';
-$plugin->requires  = 2010112400;
+$plugin->requires  = 2011120100;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release = '2.1.2 (Build: 2011120600)';
