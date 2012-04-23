b2300282610c723d96005e4168b756d0c4da7415
Danny Wahl <danny@iywahl.com>
2012-04-23 10:06:51 +0800

    whitespace cleanup

--- db/upgrade.php
+++ db/upgrade.php
@@ -26,46 +26,30 @@ defined('MOODLE_INTERNAL') || die();
 
 function xmldb_theme_zebra_upgrade($oldversion) {
     global $CFG, $DB;
-
     $dbman = $DB->get_manager();
 
     if ($oldversion < 2011111004) { // New Settings in 2.1
         $currentsetting = get_config('theme_zebra');
+        set_config('linkcolor', $currentsetting->firstcolor, 'theme_zebra'); // Create linkcolor
+        unset_config('firstcolor', 'theme_zebra'); // Remove firstcolor
 
-        // Create linkcolor
-        set_config('linkcolor', $currentsetting->firstcolor, 'theme_zebra');
-        // Remove firstcolor
-        unset_config('firstcolor', 'theme_zebra');
+        set_config('hovercolor', $currentsetting->secondcolor, 'theme_zebra'); // Create hovercolor
+        unset_config('secondcolor', 'theme_zebra'); // Remove secondcolor
 
-        // Create hovercolor
-        set_config('hovercolor', $currentsetting->secondcolor, 'theme_zebra');
-        // Remove secondcolor
-        unset_config('secondcolor', 'theme_zebra');
+        set_config('fontcolor', $currentsetting->thirdcolor, 'theme_zebra'); // Create fontcolor
+        unset_config('third', 'theme_zebra'); // Remove thirdcolor
 
-        // Create fontcolor
-        set_config('fontcolor', $currentsetting->thirdcolor, 'theme_zebra');
-        // Remove thirdcolor
-        unset_config('third', 'theme_zebra');
+        set_config('hovercolor', $currentsetting->fourthcolor, 'theme_zebra'); // Create contentbgcolor
+        unset_config('fourthcolor', 'theme_zebra'); // Remove fourthcolor
 
-        // Create contentbgcolor
-        set_config('hovercolor', $currentsetting->fourthcolor, 'theme_zebra');
-        // Remove fourthcolor
-        unset_config('fourthcolor', 'theme_zebra');
+        set_config('columnbgcolor', $currentsetting->fifthcolor, 'theme_zebra'); // Create columnbgcolor
+        unset_config('fifthcolor', 'theme_zebra'); // Remove fifthcolor
 
-        // Create columnbgcolor
-        set_config('columnbgcolor', $currentsetting->fifthcolor, 'theme_zebra');
-        // Remove fifthcolor
-        unset_config('fifthcolor', 'theme_zebra');
+        set_config('headerbgcolor', $currentsetting->sixthcolor, 'theme_zebra'); // Create headerbgcolor
+        unset_config('sixthcolor', 'theme_zebra'); // Remove sixthcolor
 
-        // Create headerbgcolor
-        set_config('headerbgcolor', $currentsetting->sixthcolor, 'theme_zebra');
-        // Remove sixthcolor
-        unset_config('sixthcolor', 'theme_zebra');
-
-        // Create footerbgcolor
-        set_config('footerbgcolor', $currentsetting->seventhcolor, 'theme_zebra');
-        // Remove seventhcolor
-        unset_config('seventhcolor', 'theme_zebra');
+        set_config('footerbgcolor', $currentsetting->seventhcolor, 'theme_zebra'); // Create footerbgcolor
+        unset_config('seventhcolor', 'theme_zebra'); // Remove seventhcolor
 
         // Upgrade version number
         upgrade_plugin_savepoint(true, 2011111004, 'theme', 'zebra');
@@ -73,48 +57,32 @@ function xmldb_theme_zebra_upgrade($oldversion) {
 
     if ($oldversion < 2011120500) { // New Settings in 2.1.1
         $currentsetting = get_config('theme_zebra');
+        unset_config('onecolmax', 'theme_zebra'); // Remove onecolmax
+        unset_config('twocolmax', 'theme_zebra'); // Remove twocolmax
 
-        // Remove onecolmax
-        unset_config('onecolmax', 'theme_zebra');
-
-        // Remove twocolmax
-        unset_config('twocolmax', 'theme_zebra');
-
-        // Create pagemaxwidth
-        set_config('pagemaxwidth', $currentsetting->threecolmax, 'theme_zebra');
-        // Remove threecolmax
-        unset_config('threecolmax', 'theme_zebra');
+        set_config('pagemaxwidth', $currentsetting->threecolmax, 'theme_zebra'); // Create pagemaxwidth
+        unset_config('threecolmax', 'theme_zebra'); // Remove threecolmax
     }
 
-	if ($oldversion < 2012011500) { // New Settings in 2.2.0
-		$currentsetting = get_config('theme_zebra');
+    if ($oldversion < 2012011500) { // New Settings in 2.2.0
+	$currentsetting = get_config('theme_zebra');
+	set_config('userespond', 0, 'theme_zebra'); // Create userespond
+	set_config('usecf', 0, 'theme_zebra'); // Create usecf
+	set_config('maxcfversion', 6, 'theme_zebra'); // Create maxcfversion
+	upgrade_plugin_savepoint(true, 2012011500, 'theme', 'zebra');
+    }
 
-		// Create userespond
-		set_config('userespond', 0, 'theme_zebra');
+    if ($oldversion < 2012011501) { // New Settings in 2.2.0
+        unset_config('enablezoom', 'theme_zebra'); // Remove enablezoom
+	upgrade_plugin_savepoint(true, 2012011501, 'theme', 'zebra');
+    }
 
-		// Create usecf
-		set_config('usecf', 0, 'theme_zebra');
-
-		// Create maxcfversion
-		set_config('maxcfversion', 6, 'theme_zebra');
-
-		upgrade_plugin_savepoint(true, 2012011500, 'theme', 'zebra');
-	}
-
-	if ($oldversion < 2012011501) { // New Settings in 2.2.0
-
-        // Remove enablezoom
-        unset_config('enablezoom', 'theme_zebra');
-
-		upgrade_plugin_savepoint(true, 2012011501, 'theme', 'zebra');
-	}
-
-	if ($oldversion < 2012042300) { // New Settings in 2.2.5
-	    $currentsetting = get_config('theme_zebra');
-	    unset_config('colwidth', 'theme_zebra'); //Remove the old colwidth
-	    set_config('colwidth', '200px', 'theme_zebra'); //Set the new colwidth
-	    upgrade_plugin_savepoint(true, 2012042300, 'theme', 'zebra');
-	}
+    if ($oldversion < 2012042300) { // New Settings in 2.2.5
+	$currentsetting = get_config('theme_zebra');
+	unset_config('colwidth', 'theme_zebra'); //Remove the old colwidth
+	set_config('colwidth', '200px', 'theme_zebra'); //Set the new colwidth
+	upgrade_plugin_savepoint(true, 2012042300, 'theme', 'zebra');
+    }
 
     return true;
 }
