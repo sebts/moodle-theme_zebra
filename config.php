c8adc943b1dd7f32f4b0df07246125eba911eeb6
Danny Wahl <danny@iywahl.com>
2012-05-09 10:00:16 +0800

    pull autohide css out of lib.php by using @import()

--- config.php
+++ config.php
@@ -44,7 +44,8 @@ $THEME->sheets = array(
     'dock',         //Derived from Rebase
     'mod_forum',    //Overrides for forum
     'extra',        //Extra stuff that doesn't fit the above
-    'ie'           //Special Internet Explorer rules
+    'ie',            //Special Internet Explorer rules
+    'import'        //Special import rules that come from lib
 );
 
 $THEME->layouts = array(
