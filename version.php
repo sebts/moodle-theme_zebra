53cc8bb357bcb2e1b5541f0d11f98da19f032dee
Danny Wahl <danny@iywahl.com>
2012-05-09 10:16:46 +0800

    version bump to 2.2.9

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012050900;
+$plugin->version   = 2012050901;
 $plugin->component = 'theme_zebra';
 $plugin->requires  = 2011120100;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '2.2.8 (Build: 2012050900)';
+$plugin->release = '2.2.9 (Build: 2012050901)';
