7e90f79472dc0e3f9ada2b67c6d51f1caeb83d81
Danny Wahl <danny@iywahl.com>
2012-09-28 15:34:12 +0800

    minor version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012092800;
+$plugin->version   = 2012092801;
 $plugin->component = 'theme_zebra';
 $plugin->requires  = 2010112400;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.3.1 (Build: 2012092800)';
+$plugin->release   = '2.3.1 (Build: 2012092801)';
