d760e63de19baba547e9a82ecb54308943989d91
Danny Wahl <danny@iywahl.com>
2012-09-28 15:29:07 +0800

    remove more unnecessary functions

--- lib.php
+++ lib.php
@@ -31,14 +31,6 @@
  */
 function zebra_process_css($css, $theme) {
 
-    //Get the path to the logo url from settings
-    if (!empty($theme->settings->logourl)) {
-        $logourl = $theme->settings->logourl;
-    } else {
-        $logourl = null;
-    }
-    $css = zebra_set_logourl($css, $logourl);
-
     //Get the path to the background url from settings
     if (!empty($theme->settings->backgroundurl)) {
         $backgroundurl = $theme->settings->backgroundurl;
@@ -286,38 +278,6 @@ function zebra_process_css($css, $theme) {
 
     return $css;
 };
-
-/**
- * Sets the logo url for the header
- *
- * @param string $css
- * @param mixed $logourl
- * @return string
- */
-function zebra_set_logourl($css, $logourl) {
-    global $OUTPUT;
-    $tag = '[[setting:logourl]]';
-    if (is_null($logourl)) {
-        $replacement = $OUTPUT->pix_url('logo/logo', 'theme'); //Default image
-    }
-    else {
-       $protocol = '://';
-        $ntp = strpos($logourl, $protocol); // Check to see if a networking protocol is used
-        if($ntp === false) { // No networking protocol used
-            $relative = '/';
-            $rel = strpos($logourl, $relative); // Check to see if a relative path is used
-            if($rel !== 0) { // Doesn't start with a slash
-                $replacement = $OUTPUT->pix_url("$logourl", 'theme'); // Using Moodle output
-            } else {
-                $replacement = $logourl;
-            }
-        } else {
-            $replacement = $logourl;
-        }
-    }
-    $css = str_replace($tag, $replacement, $css);
-    return $css;
-}
 
 /**
  * Sets the body background image url
