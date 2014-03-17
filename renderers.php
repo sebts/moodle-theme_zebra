8d465eaf0f5c2a963b3ec93db95634e115b022ec
Bryan Underwood <bunderwood@sebts.edu>
2014-03-17 16:58:50 -0400

    add STR 4.2 to top navigation

--- renderers.php
+++ renderers.php
@@ -92,6 +92,7 @@ class theme_zebra_core_renderer extends core_renderer {
 			$branch->add("STR 3.1 (2012)", new moodle_url('/mod/resource/view.php?inpopup=true&id=62328'), "STR 3.1 (2012)");
 			$branch->add("STR 3.2 (2012)", new moodle_url('/mod/page/view.php?id=84134'), "STR 3.2 (2012)");
 			$branch->add("STR 4.1 (2013)", new moodle_url('/mod/page/view.php?id=103550'), "STR 4.1 (2013)");
+            $branch->add("STR 4.2 (2013)", new moodle_url('/mod/page/view.php?id=126414'), "STR 4.2 (2013)");
 		}
 
 		$branch = $menu->add('HELP', NULL, 'Help', 90000);
